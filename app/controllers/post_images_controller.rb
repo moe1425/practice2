c23310744ebb207617dd5c345050733b0ba16410
EC2 Default User <piyoreo.1007@gmail.com>
2022-10-16 06:29:14 +0000

    create追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,6 +2,13 @@ class PostImagesController < ApplicationController
   def new
     @post_images = PostImage.new
   end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
 
   def show
   end
