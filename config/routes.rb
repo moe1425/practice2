9a2a21e96f4b38d78b83ef8c4c378b41f32c93e6
EC2 Default User <piyoreo.1007@gmail.com>
2022-10-22 06:28:53 +0000

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   devise_for :users
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   
   get "/homes/about" => "homes#about", as: "about"
 end
