7a3a204d6c82e0a2de60a4c1ed0af60b5c9c7621
EC2 Default User <piyoreo.1007@gmail.com>
2022-10-22 06:27:47 +0000

    controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   devise_for :users
