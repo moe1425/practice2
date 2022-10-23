a7b5c8dd7c6061b2c1a74dbc9f6bffd8206411ce
EC2 Default User <piyoreo.1007@gmail.com>
2022-10-23 02:41:28 +0000

    userのコントローラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
   class UsersController < ApplicationController
     
     def show
-      @user = User.find
+      @user = User.find(user_params)
       @post_images = @user.post_images
     end
   
     def edit
-      @user = User.
+      @user = User.find(user_params)
+    end
+    
+    private
+   
+    def user_params
+      params.require(:user).permit(:name, :profile_image)
     end
   end
