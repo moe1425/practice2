030d34d1fca243db8888ed6f21c604b579a0e8b4
EC2 Default User <piyoreo.1007@gmail.com>
2022-10-26 03:45:19 +0000

    update追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,12 @@
       @user = User.find(user_params)
     end
     
+    def update
+      @user = User.find(user_params)
+      @user.update
+      redirect_to user_path
+    end
+    
     private
    
     def user_params
